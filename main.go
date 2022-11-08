ac4f1b86361d9d26f643e0c6cff8985013eead8c
adityapatelcx <aditya.patel@cx100.com>
2022-11-08 13:23:03 +0530

    mongo-golang integration

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/adityapatelcx/mongo-golang/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main(){
+
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+
+func getSession() *mgo.Session{
+
+	s, err := mgo.Dial("mongodb://aditya:aditya@cluster0.oic6x.mongodb.net/")
+	if err != nil{
+		panic(err)
+	}
+	return s
+}
